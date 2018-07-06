05da62fae1be638f76c3c510a4dd6198ce7c9e40
sjchoi <sjchoi@polycube.co.kr>
2018-07-06 23:33:54 +0900

    Initial Commit

---
+++ toyproject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  toyproject
+//
+//  Created by polycube on 2018. 7. 6..
+//  Copyright © 2018년 sesang06. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
